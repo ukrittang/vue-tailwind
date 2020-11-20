63e65d8b861f9540404f07115f894b82088c8867
Tang <mrgaoyi@gmail.com>
2020-11-20 12:44:07 +0700

    add custom font loader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="text-red-600">
+  <div class="text-red-600 font-baloo">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
+
+<script>
+import WebFont from "webfontloader";
+
+export default {
+  name: "App",
+  created() {
+    WebFont.load({
+      google: {
+        families: ["Baloo 2:400,600"],
+      },
+    });
+  },
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const sans = [
+  '"Baloo 2"',
+  "system-ui",
+  "-apple-system",
+  '"Segoe UI"',
+  "Roboto",
+  "Ubuntu",
+  "Cantarell",
+  '"Noto Sans"',
+  "sans-serif",
+  "BlinkMacSystemFont",
+  '"Segoe UI"',
+  "Roboto",
+  '"Helvetica Neue"',
+  "Arial",
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Noto Color Emoji"',
+];
+
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        baloo: ['"Baloo 2"', ...sans],
+      },
+    },
   },
   variants: {
     extend: {},
